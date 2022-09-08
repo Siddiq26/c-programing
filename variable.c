878c3d17a18471ee428551dface16cb4685d9be9
Siddiq26 <98523686+Siddiq26@users.noreply.github.com>
2022-09-08 12:56:23 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+   printf("welcome to c programming...");
+   int age=50;
+       char name[10]="siddiq";
+       printf("\n my name is..%s",name);
+       printf("\n my age is..%d",age);
+       char c='a';
+       char cc=100;
+       const double pi=3.14;
+       // pie=50;
+       printf("\t my pi vale is..%if",pi);
+       printf("\n the size of pi variable is...%d",sizeof(pi));
+}
